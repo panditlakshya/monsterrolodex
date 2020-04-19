8dadceed669914e17ba7dc4aafc1e8fe16114e2c
Lakshya <lakshyapandit2054@gmail.com>
2020-04-19 18:23:41 +0545

    First commit

---
+++ build/precache-manifest.0049705a535e369204192ce451955a6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a697461f071c46077d2465a7f06a115",
+    "url": "/monsterrolodex/index.html"
+  },
+  {
+    "revision": "cc0f3ec37b2f28461335",
+    "url": "/monsterrolodex/static/css/main.4a7fdb8a.chunk.css"
+  },
+  {
+    "revision": "378f6e3f28ad625fb757",
+    "url": "/monsterrolodex/static/js/2.adf19a6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterrolodex/static/js/2.adf19a6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0f3ec37b2f28461335",
+    "url": "/monsterrolodex/static/js/main.e2cc7cba.chunk.js"
+  },
+  {
+    "revision": "52dd2cd96d1e671fb6b0",
+    "url": "/monsterrolodex/static/js/runtime-main.17d3bf5d.js"
+  }
+]);
